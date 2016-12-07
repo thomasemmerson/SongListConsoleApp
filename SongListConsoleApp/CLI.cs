b183f78223f67c2c0424b8418f0e5311ac9e1090
Tom Emmerson <thomasemmerson@users.noreply.github.com>
2016-12-07 17:02:57 +0000

    First remote commit

---
+++ SongListConsoleApp/CLI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongListConsoleApp
+{
+    class CLI
+    {
+        internal static void DisplayWelcome()
+        {
+            Console.WriteLine("----------Song List-----------");
+            Console.WriteLine("A List of your Favourite Songs");
+            Console.WriteLine("------------------------------");
+        }
+
+        internal static string Prompt(string message)
+        {
+            Console.Write(message);
+            string userInput = Console.ReadLine();
+            Console.WriteLine();
+
+            return userInput.Trim();
+        }
+    }
+}
---
+++ SongListConsoleApp/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongListConsoleApp
+{
+    class Song
+    {
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " by " + Artist;
+        }
+    }
+}
