cb5c606b52a901033d46ed18cf7c7abc72b3fc62
Tom Emmerson <thomasemmerson@users.noreply.github.com>
2016-12-07 17:07:29 +0000

    Further commit

---
+++ SongListConsoleApp/SongListConsoleApp/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongListConsoleApp
+{
+    internal class Menu
+    {
+        private static string[] _options = new string[]
+        {
+            "Add song",
+            "View list",
+            "Quit"
+        };
+
+        static void Display()
+        {
+            for (int i = 0; i < _options.Length; i++)
+            {
+                Console.WriteLine($"{i+1}) {_options[i]}");
+            }
+        }
+    }
+}
---
+++ SongListConsoleApp/SongListConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongListConsoleApp
+{
+    public class Program
+    {
+        static List<Song> _songs = new List<Song>();
+
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            CLI.DisplayWelcome();
+
+            int option = 0;
+            while ((option = CLI.Prompt()) != 3)
+            {
+                switch (option)
+                {
+                    case 1:
+                        AddSong();
+                        break;
+                    case 2:
+                        DisplaySongList();
+                        break;
+                }
+            }
+        }
+
+        static void DisplaySongList()
+        {
+            Console.WriteLine("Songs");
+            Console.WriteLine("-----------");
+            _songs.ForEach((song) => Console.WriteLine(song));
+            Console.WriteLine();
+        }
+
+        static void AddSong()
+        {
+            bool done = false;
+            do
+            {
+                string songName = CLI.Prompt("What's the name of the song? ");
+                string artist = CLI.Prompt("Who's the artist? ");
+
+                _songs.Add(new Song { Name = songName, Artist = artist });
+                done = CLI.Prompt("Add another song? (Y/N) ").ToLower() != "y";
+
+            } while (!done);
+        }
+    }
+}
